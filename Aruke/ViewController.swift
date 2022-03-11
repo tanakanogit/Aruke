dea062967b5cadf6054756039d11291528a86700
ttanaka <ttanaka@fourmix.co.jp>
2022-03-11 16:21:40 +0900

    Initial Commit

---
+++ Aruke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aruke
+//
+//  Created by ttanaka on 2022/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
